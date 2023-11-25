cc41875cea961cf77ea752b48b33b91f0a6fe363
shidqi29 <shidqiamirulhaq@gmail.com>
2023-11-25 22:45:59 +0700

    feat: add profile summary

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,50 @@
+import { Separator } from "@/components/ui/separator";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { socials } from "@/constant/socials";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Next.js + TypeScript + Tailwind CSS + shadcn/ui</h1>
+    <div className="mx-auto flex h-screen w-full max-w-lg flex-col items-center justify-center">
+      <div className="flex w-full flex-col gap-y-1 text-4xl font-semibold md:text-5xl">
+        <h1>Hello, I&apos;m Shidqi</h1>
+        <span className="from-secondary-foreground to-accent bg-gradient-to-bl bg-clip-text text-transparent">
+          Front-End Developer.
+        </span>
+      </div>
+      <div className="mt-5">
+        <p className="text-justify text-lg text-zinc-400">
+          Specializing in crafting responsive and engaging user interfaces using
+          ReactJS. Enthusiastic about learning new technologies and committed to
+          delivering an outstanding user experience.
+        </p>
+      </div>
+      <Separator className="my-4" />
+      <div className="flex w-full flex-wrap gap-x-4">
+        {socials.map((social) => (
+          <a
+            key={social.name}
+            href={social.url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Tooltip>
+              <TooltipTrigger>
+                <social.Icon
+                  size={28}
+                  className="hover:text-accent transition-colors"
+                />
+              </TooltipTrigger>
+              <TooltipContent>
+                <span>{social.name}</span>
+              </TooltipContent>
+            </Tooltip>
+          </a>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/constant/socials.ts
@@ -0,0 +1,19 @@
+import { Linkedin, Github, Instagram } from "lucide-react";
+
+export const socials = [
+  {
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/in/shidqiamirulhaq/",
+    Icon: Linkedin,
+  },
+  {
+    name: "Github",
+    url: "https://github.com/shidqi29",
+    Icon: Github,
+  },
+  {
+    name: "Instagram",
+    url: "https://www.instagram.com/shidqiamrlhaq/",
+    Icon: Instagram,
+  },
+];
